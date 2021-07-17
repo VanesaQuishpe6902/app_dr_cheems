b3cafae088bce897080917d32bd9714bb57e9046
allisonvanessa97 <allisonvanessa97@gmail.com>
2021-07-16 22:29:55 -0500

    Creacion de interfaces  VQuishpe

---
+++ app/src/main/java/com/utc/drcheems/usu_registro.java
@@ -0,0 +1,14 @@
+package com.utc.drcheems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class usu_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usu_registro);
+    }
+}
