557105b701e3f657bf2684ed116ff1913648da48
allisonvanessa97 <allisonvanessa97@gmail.com>
2021-07-17 22:59:36 -0500

    Creacion de interfaces  VQuishpe

---
+++ app/src/main/java/com/utc/drcheems/usu_cambiar_contrasena.java
@@ -0,0 +1,14 @@
+package com.utc.drcheems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class usu_cambiar_contrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usu_cambiar_contrasena);
+    }
+}
